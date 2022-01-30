eaeaa8413a954424e7d0c17bd164dca6e48e1f85
892280082 <892280082@qq.com>
2022-01-30 16:49:54 +0800

    change

--- ApplicationBoot.js
+++ ApplicationBoot.js
@@ -1,5 +1,23 @@
 const {SpringBoot} = require("./Spring")
 
+
+/** 支持修改的参数
+args = {
+	rootPath:"", //项目根路径
+	srcList:[],  //源码目录集合
+	tempJsName:".runtemp.js", //生成临时文件名称
+	resourceDir:"resource", //资源目录名称
+	annotation:{ //配置系统默认注解
+		valueInject:"Value",
+		appBoot:"SpringBoot",
+		beanInject:"Autowired",
+		springFactory:"SpringFactory",
+		springResource:"SpringResource",
+		proxy:"Proxy"
+	}
+}
+*/
+
 //正式使用的时候 删除packageName属性
 new SpringBoot({srcList:["./app"],packageName:"./spring"}).run();
 
