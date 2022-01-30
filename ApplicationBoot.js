5a7d28f2f7dbf1aef1d8814048c155b2af1e6093
892280082 <892280082@qq.com>
2022-01-30 16:48:08 +0800

    auto update

--- ApplicationBoot.js
+++ ApplicationBoot.js
@@ -1,4 +1,13 @@
 const {SpringBoot} = require("./Spring")
 
 //正式使用的时候 删除packageName属性
-new SpringBoot({dirList:["./app"],packageName:"./spring"}).run();
+new SpringBoot({srcList:["./app"],packageName:"./spring"}).run();
+
+
+/**
+	1.同样也支持手动启动,先用node生成.runtemp.js临时运行文件
+	new SpringBoot({srcList:["./app"]});
+	
+	2.手动启动
+	shell: node .runtemp.js
+*/
