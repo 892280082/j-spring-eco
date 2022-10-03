eb3331c9f4274d4e7a92dba918db97b90ce34321
yeqin <892280082@qq.com>
2022-10-03 18:44:21 +0800

    edit starter

--- j-spring-web/src/springWebContainer.ts
+++ j-spring-web/src/springWebContainer.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { Component, Value,SpringStarter } from "j-spring";
+import { Component, Value,SpringStarter,Clazz } from "j-spring";
 import {loadConfiguration} from './springWebBeanProcessor'
 
 @Component()
@@ -8,9 +8,13 @@ export class SpringWebStarter implements SpringStarter{
     @Value({path:'j-spring.port',type:Number,force:false})
     port = 3000;
 
-    async doStart(): Promise<any> {
+    async doStart(clazzMap: Map<Clazz, any>): Promise<any> {
         const app = express();
+        
+        clazzMap.set(express as any,app);
+
         loadConfiguration(app);
+        
         const startWeb = ()=> new Promise( (ok,_err) => {
             app.listen(this.port, () => {
                 // tslint:disable-next-line:no-console
