5c24a8f0f74e80dde35e64564cd67fe8ae93909c
yeqin <892280082@qq.com>
2022-10-12 02:39:27 +0800

    not publish src

--- j-spring/src/util/printLogo.ts
+++ j-spring/src/util/printLogo.ts
@@ -1,9 +1,22 @@
 import path from 'path';
 import fs from 'fs'
 
+//递归查询logo文件
+function findLogoFile(currentDir:string,fileName:string,deep?:number):string{
+
+    deep = deep || 0;
+
+    if(deep > 5)
+        throw `logo文件不存在`
+
+    const filePath = path.join(currentDir,fileName)
+
+    return fs.existsSync(filePath) ? filePath : findLogoFile(path.join(currentDir,'../'),fileName,deep+1);
+}
+
 export function printLogo(){
 
-    const data = fs.readFileSync(path.join(__dirname,'./app.logo'))
+    const data = fs.readFileSync(findLogoFile(path.join(__dirname,'../'),'app.logo'))
 
     console.log(data.toString())
 
