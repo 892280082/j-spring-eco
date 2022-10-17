bb41c5f753945a876fa45d57627c35fc2d026ba4
yeqin <892280082@qq.com>
2022-10-17 22:45:02 +0800

    ok

--- j-spring-web/test/blah.test.ts
+++ j-spring-web/test/blah.test.ts
@@ -1,53 +1,48 @@
-import { start ,end } from '../web/app'
-import axios from 'axios'
+import { start, end } from '../web/app';
+import axios from 'axios';
 
 describe('blah', () => {
-
-  beforeAll((done)=>{
-     start(2000).then(done)
-  })
-
-  afterAll(done=>{
-    end(done)
-  })
-
-  it('test connect',async  () => {
-
-      console.log('测试链接');
-
-      const result = await axios.get('http://localhost:2000/student/getConnectMsg')
-
-      expect(result.data).toEqual('is-ok')
-
+  beforeAll(done => {
+    start(2000).then(done);
   });
 
-  it('test send paramter',async ()=>{
+  afterAll(done => {
+    end(done);
+  });
 
-    console.log("测试传递参数");
-    
-    const result = await axios.get('http://localhost:2000/student/testParamter/123?b='+456)
+  it('test connect', async () => {
+    console.log('测试链接');
 
-    expect(result.data).toEqual('123456')
+    const result = await axios.get(
+      'http://localhost:2000/student/getConnectMsg'
+    );
 
+    expect(result.data).toEqual('is-ok');
+  });
 
-  })
+  it('test send paramter', async () => {
+    console.log('测试传递参数');
 
-  it('test shuttle api',async ()=>{
+    const result = await axios.get(
+      'http://localhost:2000/student/testParamter/123?b=' + 456
+    );
 
-    const result = await axios.post('http://localhost:2000/shuttleApi/say',{args:[2,'h']})
+    expect(result.data).toEqual('123456');
+  });
 
-    expect(result.data).toEqual('hh')
+  it('test shuttle api', async () => {
+    const result = await axios.post('http://localhost:2000/shuttleApi/say', {
+      args: [2, 'h'],
+    });
 
-  })
+    expect(result.data).toEqual('hh');
+  });
 
-  it('test testDiyMiddleWare api',async ()=>{
+  it('test testDiyMiddleWare api', async () => {
+    const result = await axios.get(
+      'http://localhost:2000/student/testDiyMiddleWare'
+    );
 
-    const result = await axios.get('http://localhost:2000/student/testDiyMiddleWare')
-
-    expect(result.data).toEqual(1)
-
-  })
-  
-
-
+    expect(result.data).toEqual(1);
+  });
 });
